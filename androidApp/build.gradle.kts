209bed01e3c797287025443cb9070c29aa8ecf5a
Pedro Frois Bittencourt <p.f.bit@hotmail.com>
2021-08-03 21:34:17 -0300

    feat: build.gradle.kts
    - dependencies
    - refac

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,21 +1,67 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("android.extensions")
+    id("kotlin-kapt")
 }
+// Android SDK Versions
+val compileSdkNumber = (findProperty(("android.compileSdk")) as String).toInt()
+val targetSdkNumber = (findProperty(("android.targetSdk")) as String).toInt()
+val minSdkNumber = (findProperty(("android.minSdk")) as String).toInt()
+// Coroutine
+val coroutineVersion = findProperty("android.kotlinx.coroutines") as String
+// UI
+val appCompatVersion = findProperty("android.appcompat") as String
+val materialVersion = findProperty("android.material") as String
+val constraintVersion = findProperty("android.constraintlayout") as String
+// Lifecycle
+val lifecycleVersion = findProperty("android.lifecycle") as String
+val lifecycleExtVersion = findProperty("android.lifecycle.extensions") as String
+// Navigation
+val navigationVersion = findProperty("android.navigation") as String
+// DI
+val koinVersion = findProperty("android.koin") as String
+// WorkManager
+val workVersion = findProperty("android.work") as String
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    // UI
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("com.google.android.material:material:$materialVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintVersion")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleExtVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
+    // DI
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-android:$koinVersion")
+
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:$workVersion")
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = compileSdkNumber
+    buildFeatures {
+        dataBinding = true
+    }
     defaultConfig {
         applicationId = "br.com.bittencourt.kmmsample.android"
-        minSdkVersion(24)
-        targetSdkVersion(30)
+        minSdk = minSdkNumber
+        targetSdk = targetSdkNumber
         versionCode = 1
         versionName = "1.0"
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 buildscript {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
     }
+
+    val kotlinVersion = findProperty("version.kotlin") as String
+    val gradlePluginVersion = findProperty("version.androidGradlePlugin") as String
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:$gradlePluginVersion")
     }
 }
 
